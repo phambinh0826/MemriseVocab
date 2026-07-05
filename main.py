74d8c588a9ffed108536340f74264aaeaeabfd73
BinhPham0826 <ptcb1012588646@gmail.com>
2026-07-06 00:48:03 +0700

    Refactor HTML structure in main.py

--- main.py
+++ main.py
@@ -6,18 +6,16 @@ def define_env(env):
     <button onclick="openFullscreen()" class="md-button md-button--primary btn-desktop" style="margin-bottom: 15px; cursor: pointer;">
       🖵 Xem Toàn Màn Hình
     </button>
-
     <object id="myPdf" data="../{ten_file}" type="application/pdf" width="100%" height="800px" class="pdf-desktop">
         <p>Trình duyệt của bạn không hỗ trợ xem PDF trực tiếp.</p>
     </object>
-
     <button onclick="openMobilePDF('{ten_file}')" class="md-button md-button--primary btn-mobile">
         📄 Chạm để Đọc PDF Toàn Màn Hình
     </button>
 </div>
 """
 
-@env.macro
+    @env.macro
     def nhung_podcast(ten_file):
         return f"""
 <div class="podcast-wrapper">
