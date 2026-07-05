ac1434eea4bbb0dfa05f84cf99670f42da3965fb
BinhPham0826 <ptcb1012588646@gmail.com>
2026-07-05 22:57:26 +0700

    Add mobile PDF viewing functionality
    Added a function to open PDF files in mobile browsers using Google Docs viewer.

--- docs/javascripts/custom.js
+++ docs/javascripts/custom.js
@@ -1,13 +1,20 @@
-// Lệnh phóng to PDF dùng chung cho toàn trang web
+// 1. Lệnh phóng to PDF trên Máy tính (Giữ nguyên)
 function openFullscreen() {
   var elem = document.getElementById("myPdf");
   if (elem) {
-      if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-      } else if (elem.webkitRequestFullscreen) { 
-        elem.webkitRequestFullscreen();
-      } else if (elem.msRequestFullscreen) { 
-        elem.msRequestFullscreen();
-      }
+      if (elem.requestFullscreen) { elem.requestFullscreen(); }
+      else if (elem.webkitRequestFullscreen) { elem.webkitRequestFullscreen(); }
+      else if (elem.msRequestFullscreen) { elem.msRequestFullscreen(); }
   }
+}
+
+// 2. Lệnh ép đọc PDF trên trình duyệt Điện thoại (Mới)
+function openMobilePDF(fileName) {
+    // Lấy đường dẫn gốc của trang web hiện tại
+    var currentUrl = window.location.href;
+    // Nối tên file để ra link PDF tuyệt đối
+    var pdfUrl = new URL("../" + fileName, currentUrl).href;
+    // Mượn trình đọc PDF của Google Docs để ép hiển thị trên web, không cho tải về
+    var viewerUrl = "https://docs.google.com/viewer?url=" + encodeURIComponent(pdfUrl) + "&embedded=false";
+    window.open(viewerUrl, '_blank');
 }
