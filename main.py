39b4734c16fc6179913c984aafbdd6c6ae4d177e
BinhPham0826 <ptcb1012588646@gmail.com>
2026-07-05 22:43:14 +0700

    Add PDF display macro in main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def define_env(env):
+    @env.macro
+    def nhung_pdf(ten_file):
+        return f"""
+<div class="pdf-wrapper">
+    <button onclick="openFullscreen()" class="md-button md-button--primary btn-desktop" style="margin-bottom: 15px; cursor: pointer;">
+      🖵 Xem Toàn Màn Hình
+    </button>
+
+    <object id="myPdf" data="../{ten_file}" type="application/pdf" width="100%" height="800px" class="pdf-desktop">
+        <p>Trình duyệt của bạn không hỗ trợ xem PDF trực tiếp.</p>
+    </object>
+
+    <a href="../{ten_file}" target="_blank" class="md-button md-button--primary btn-mobile">
+        📄 Chạm để Đọc PDF Toàn Màn Hình
+    </a>
+</div>
+"""
